f6df22611ceb407964daf50731e8b9243e67e8ce
yvadher <yagniktechmail@gmail.com>
2020-05-28 16:15:35 +0530

    Added herurstics functions and statstics for report

--- a1.py
+++ a1.py
@@ -6,17 +6,27 @@ Assignment 1 file for running 8-Puzzel and finding the A* algortihym optimality
 import random
 import time
 from utils import memoize, PriorityQueue
-from search import EightPuzzle, Node
+from search import EightPuzzle, Node, Problem
 
 class EightPuzzleGame(EightPuzzle):
 
     def __init__(self, initial):
         super().__init__(initial)
 
+    def calculateMissplacedTiles(self, node):
+        return sum(s != g for (s, g) in zip(node.state, self.goal) if s != 0)
+
 
     def calculateManhattenDist(self,node):
-        return sum(abs((val-1)%3 - i%3) + abs((val-1)//3 - i//3) for i, val in enumerate(node.state) if val)
+        h_value = 0
+        for i, number in enumerate(node.state):
+            if number:
+                h_value += abs((number-1)%3 - i%3) + abs((number-1)//3 - i//3)
+        return h_value
 
+    def maxH(self, node):
+        return max(self.calculateManhattenDist(node), self.calculateMissplacedTiles(node))
+        
 
     def display(self):
          #clousure function 
@@ -27,25 +37,25 @@ class EightPuzzleGame(EightPuzzle):
             printer(number) if (number != 0 ) else printer("*")
             printer("\n") if ( (index + 1) % 3 == 0 and index != 0 ) else printer(" ")
 
+
+
 ## below function code is from the search.py with some modification. 
 def astar_search(problem, h, display=False):
 
     h = memoize(h or problem.h, 'h')
     f = memoize(lambda n: n.path_cost + h(n), 'f')
-
     node = Node(problem.initial)
     frontier = PriorityQueue('min', f)
     frontier.append(node)
     explored = set()
     frontierPopCount = 0
     start_time = time.time()
-    
     while frontier:
         node = frontier.pop()
         frontierPopCount += 1
         if problem.goal_test(node.state):
             if display:
-                print("Paths in frontier :", str(node.depth), " | ", str(frontierPopCount))
+                print("Sln Length :", str(node.depth), " | removed nodes from frontier:  ", str(frontierPopCount))
                 elapsed_time = time.time() - start_time
                 print(f'elapsed time (in seconds): {elapsed_time}s')
             return node
@@ -60,7 +70,6 @@ def astar_search(problem, h, display=False):
     return None
 
 
-
 def display(state):
     
     #clousure function 
@@ -73,14 +82,17 @@ def display(state):
             
         
 def make_rand_8puzzle():
-    # while True:
-    #     initialPuzzel = tuple(random.sample( range(0,9), 9))
-    #     puzzel = EightPuzzleGame(initialPuzzel)
-    #     if (puzzel.check_solvability(puzzel.initial)): 
-    #         return puzzel
+    while True:
+        initialPuzzel = tuple(random.sample( range(0,9), 9))
+        puzzel = EightPuzzleGame(initialPuzzel)
+        if (puzzel.check_solvability(puzzel.initial)): 
+            print (initialPuzzel)
+            return puzzel
 
-    # return EightPuzzleGame((1,0,3,4,2,6,7,5,8))
+    return EightPuzzleGame((1,0,3,4,2,6,7,5,8))
     return EightPuzzleGame( (4,1,0,7,5,3,6,8,2) )
+    return EightPuzzleGame((8,2,3,4,5,6,7,1,0))
+
 
 def palyTheGame():
     randPuzzles = []
@@ -88,10 +100,19 @@ def palyTheGame():
         tempPuzzel = make_rand_8puzzle()
         randPuzzles.append(tempPuzzel)
 
-   
+# puz = make_rand_8puzzle()
+# print ("------- Puzzel ---------")
+# puz.display()
 
+# print ("------- Misplaced tiles ----------")
+# astar_search(puz, h=puz.calculateMissplacedTiles, display=True)
 
-puz = make_rand_8puzzle()
-puz.display()
-astar_search(puz, h=None, display=True)
-# print (puz.countMisplacedTiles( puz.initial))
+# print ("\n------- Manhatten Distance ----------")
+# astar_search(puz, h=puz.calculateManhattenDist, display=True)
+
+# print ("\n------- Max of both  ----------")
+# astar_search(puz, h=puz.maxH, display=True)
+
+# print (puz.calculateManhattenDist( puz.initial))
+
+# print (puz.maxH( puz.initial))
